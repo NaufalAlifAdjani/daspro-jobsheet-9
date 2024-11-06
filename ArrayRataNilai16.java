96f62a3ffca89d7b25fd4763e41afa6f384c9cdd
NaufalAlifAdjani <naufalalifadjani12@gmail.com>
2024-11-06 21:12:32 +0700

    lulus dan tidak lulus

--- ArrayRataNilai16.java
+++ ArrayRataNilai16.java
@@ -6,6 +6,7 @@ public class ArrayRataNilai16 {
         int[] nilaiMhs = new int[10];
         double total = 0;
         double rata2;
+
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + ": ");
             nilaiMhs[i] = sc.nextInt();
@@ -13,7 +14,16 @@ public class ArrayRataNilai16 {
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
         }
+        
         rata2 = total / nilaiMhs.length;
         System.out.println("Rata-rata nilai = " + rata2);
+
+        for (int i = 0; i < nilaiMhs.length ; i++) {
+            if (nilaiMhs[i] > 70) {
+                System.out.println("Mahasiswa ke-"+i+" lulus!");
+            }else{
+                System.out.println("Mahasiswa ke-"+i+" Tidak lulus!");
+            }
+        }
     }
 }
