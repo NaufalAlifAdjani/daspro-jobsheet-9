f6f34669e352784fcea898aea3f31640d97d72e4
NaufalAlifAdjani <naufalalifadjani12@gmail.com>
2024-11-06 21:18:54 +0700

    rata rata nilai lulus dan tidak lulus

--- ArrayRataNilai16.java
+++ ArrayRataNilai16.java
@@ -4,26 +4,29 @@ public class ArrayRataNilai16 {
         Scanner sc = new Scanner(System.in);
         
         int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+
+        double nilaiLulus = 0, nilaiTidakLulus = 0, rataLulus, rataTidakLulus;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + ": ");
             nilaiMhs[i] = sc.nextInt();
         }
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-        }
-        
-        rata2 = total / nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
 
         for (int i = 0; i < nilaiMhs.length ; i++) {
-            if (nilaiMhs[i] > 70) {
-                System.out.println("Mahasiswa ke-"+i+" lulus!");
-            }else{
-                System.out.println("Mahasiswa ke-"+i+" Tidak lulus!");
+            if (nilaiMhs[i] >= 70) {
+                lulus++;
+                nilaiLulus += nilaiMhs[i];
+            } else {
+                tidakLulus++;
+                nilaiTidakLulus += nilaiMhs[i];
             }
         }
+
+        rataLulus = nilaiLulus / lulus;
+        rataTidakLulus = nilaiTidakLulus / tidakLulus;
+
+        System.out.println("Rata - rata nilai lulus = " +rataLulus);
+        System.out.println("Rata - rata nilai lulus = " +rataTidakLulus);
     }
 }
